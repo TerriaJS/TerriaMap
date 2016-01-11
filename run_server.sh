1c1bb861893297da44cd19d0a1a36073c9df7e8d
Steve Bennett <stevage@gmail.com>
2016-01-11 18:43:40 +1100

    Remove Supervisor and Forever, rework scripts.

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
-forever stopall
-sudo /etc/init.d/varnish restart
-nohup forever node_modules/terriajs-server/app.js > output.log 2> error.log < /dev/null &
-
+if [ -f "/etc/init.d/varnish" ]; then
+    sudo /etc/init.d/varnish restart
+fi
+if [ -f terriajs.pid ]; then
+    echo "Warning: server seems to be already running."
+fi
+# pkill -f terriajs-server
+date > output.log
+nohup node node_modules/terriajs-server "$@" >> output.log 2> error.log < /dev/null &
+sleep 2 # Give the server a chance to fail.
+cat output.log 
+pid=$!
+ps | grep "^\s*${pid}" > /dev/null && echo "(TerriaJS-Server running in background with pid $!)." && echo $pid > terriajs.pid
+cat error.log
