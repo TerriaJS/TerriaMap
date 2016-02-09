9631758736ebdb14b8db88b1b09e3e0cd2cdd975
Steve Bennett <stevage@gmail.com>
2016-02-09 13:36:02 +1100

    Move all csv-geo-au showcase files into this repo. Fix CED and LGA 2011-15. Update test.json. Refine some of the showcase files to show names, not random numbers.

--- scripts/generate-region-ids.sh
+++ scripts/generate-region-ids.sh
@@ -20,7 +20,7 @@ function makeJSON {
 
     echo "$LAYER, $COLUMN"
 
-    wget  --quiet -O response "http://geoserver.nationalmap.nicta.com.au/region_map/ows?service=wfs&version=2.0&request=getPropertyValue&typenames=${LAYERU}&valueReference=${COLUMN}"
+    wget  --quiet -O response "http://regionmap-dev.nationalmap.nicta.com.au/region_map/ows?service=wfs&version=2.0&request=getPropertyValue&typenames=${LAYERU}&valueReference=${COLUMN}"
 
     # Add commas between items
     perl -pi -e "s/<\/wfs:member><wfs:member>/${QUOTES},${QUOTES}/g" response
@@ -41,6 +41,11 @@ echo "Generating region ID lists and saving to ${OUTPUTDIR}."
 mkdir -p $OUTPUTDIR
 # In an ideal world, we'd run this automatically off regionmapping.json
 
+
+makeJSON "region_map:FID_CED_2013_AUST" CED_CODE13
+makeJSON "region_map:FID_CED_2013_AUST" CED_NAME13 '"'
+
+exit
 makeJSON "region_map:FID_SA1_2011_AUST" SA1_MAIN11
 makeJSON "region_map:FID_SA1_2011_AUST" SA1_7DIG11
 makeJSON "region_map:FID_SA2_2011_AUST" SA2_MAIN11
@@ -53,6 +58,9 @@ makeJSON "region_map:FID_SA4_2011_AUST" SA4_NAME11 '"'
 makeJSON "region_map:FID_LGA_2015_AUST" LGA_CODE15
 makeJSON "region_map:FID_LGA_2015_AUST" LGA_NAME15 '"'
 makeJSON "region_map:FID_LGA_2015_AUST" STE_NAME15 '"' # disambiguation column
+makeJSON "region_map:FID_LGA_2013_AUST" LGA_CODE13
+makeJSON "region_map:FID_LGA_2013_AUST" LGA_NAME13 '"'
+makeJSON "region_map:FID_LGA_2013_AUST" STE_NAME13 '"' # disambiguation column
 makeJSON "region_map:FID_LGA_2011_AUST" LGA_CODE11
 makeJSON "region_map:FID_LGA_2011_AUST" LGA_NAME11 '"'
 makeJSON "region_map:FID_LGA_2011_AUST" STE_NAME11 '"' # disambiguation column
@@ -61,9 +69,10 @@ makeJSON "region_map:FID_SSC_2011_AUST" SSC_NAME '"'
 makeJSON "region_map:FID_POA_2011_AUST" POA_CODE '"' # leading zeroes
 makeJSON "region_map:FID_CED_2011_AUST" CED_CODE
 makeJSON "region_map:FID_CED_2011_AUST" CED_NAME '"'
+makeJSON "region_map:FID_CED_2013_AUST" CED_CODE13
+makeJSON "region_map:FID_CED_2013_AUST" CED_NAME13 '"'
 makeJSON "region_map:FID_SED_2011_AUST" SED_CODE
 makeJSON "region_map:FID_SED_2011_AUST" SED_NAME '"'
-
 makeJSON "region_map:FID_STE_2011_AUST" STE_CODE11
 makeJSON "region_map:FID_STE_2011_AUST" STE_NAME11 '"' 
 makeJSON "region_map:FID_TM_WORLD_BORDERS" ISO2 '"'
