fe642d1285c9b74332a5fc449c6cf0506cf7f8e7
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2016-08-17 15:06:18 +1000

    Recent commits to TerriaMap have introduced a dependency on the latest TerriaJS. Now pointing to github TerriaJS. Also changes to be compatible with the latest version of TerriaJS.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ import updateApplicationOnMessageFromParentWindow from 'terriajs/lib/ViewModels/
 import ViewState from 'terriajs/lib/ReactViewModels/ViewState';
 import BingMapsSearchProviderViewModel from 'terriajs/lib/ViewModels/BingMapsSearchProviderViewModel.js';
 import GazetteerSearchProviderViewModel from 'terriajs/lib/ViewModels/GazetteerSearchProviderViewModel.js';
-import GNAFSearchProviderViewModel from 'terriajs/lib/ViewModels/GNAFSearchProviderViewModel.js';
+import GnafSearchProviderViewModel from 'terriajs/lib/ViewModels/GnafSearchProviderViewModel.js';
 
 import render from './lib/Views/render';
 
@@ -95,7 +95,7 @@ terria.start({
                 key: configuration.bingMapsKey
             }),
             new GazetteerSearchProviderViewModel({terria}),
-            new GNAFSearchProviderViewModel({terria})
+            new GnafSearchProviderViewModel({terria})
         ];
 
         // Automatically update Terria (load new catalogs, etc.) when the hash part of the URL changes.
