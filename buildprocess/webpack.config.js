TerriaJS/nationalmap

Update build config for webpack2

buildprocess/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = function(devMode, hot) {
     var config = {
         entry: './index.js',
         output: {
-            path: 'wwwroot/build',
+            path: path.resolve(__dirname, 'wwwroot', 'build'),
             filename: 'nationalmap.js',
             // work around chrome needing the full URL when using sourcemaps (http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809)
             publicPath: hot ? 'http://localhost:3003/build/' : 'build/',
@@ -22,7 +22,7 @@ module.exports = function(devMode, hot) {
                 {
                     test: /\.html$/,
                     include: path.resolve(__dirname, '..', 'lib', 'Views'),
-                    loader: require.resolve('raw-loader')
+                    loader: 'raw-loader'
                 },
                 {
                     test: /\.(js|jsx)$/,
@@ -30,19 +30,19 @@ module.exports = function(devMode, hot) {
                         path.resolve(__dirname, '..', 'index.js'),
                         path.resolve(__dirname, '..', 'lib')
                     ],
-                    loader: require.resolve('babel-loader'),
+                    loader: 'babel-loader',
                     query: {
                         sourceMap: false, // generated sourcemaps are currently bad, see https://phabricator.babeljs.io/T7257
                         presets: ['es2015', 'react'],
                         plugins: [
-                            require.resolve('jsx-control-statements')
+                            'jsx-control-statements'
                         ]
                     }
                 },
                 {
                     test: /\.(png|jpg|svg|gif)$/,
                     include: path.resolve(__dirname, '..', 'wwwroot', 'images'),
-                    loader: require.resolve('url-loader'),
+                    loader: 'url-loader',
                     query: {
                         limit: 8192
                     }
@@ -50,19 +50,19 @@ module.exports = function(devMode, hot) {
                 {
                     test: /\.scss$/,
                     include: [path.resolve(__dirname, '..', 'lib')],
-                    loader: hot ?
-                        require.resolve('style-loader') + '!' +
-                        require.resolve('css-loader') + '?sourceMap&modules&camelCase&localIdentName=nm-[name]__[local]&importLoaders=2!' +
-                        require.resolve('resolve-url-loader') + '?sourceMap!' +
-                        require.resolve('sass-loader') + '?sourceMap'
-                     : ExtractTextPlugin.extract(
-                        require.resolve('css-loader') + '?sourceMap&modules&camelCase&localIdentName=nm-[name]__[local]&importLoaders=2!' +
-                        require.resolve('resolve-url-loader') + '?sourceMap!' +
-                        require.resolve('sass-loader') + '?sourceMap',
-                        {
-                            publicPath: ''
-                        }
-                    )
+                    loader: hot ? [
+                        'style-loader',
+                        'css-loader?sourceMap&modules&camelCase&localIdentName=nm-[name]__[local]&importLoaders=2',
+                        'resolve-url-loader?sourceMap',
+                        'sass-loader?sourceMap'
+                    ] : ExtractTextPlugin.extract({
+                        use: [
+                            'css-loader?sourceMap&modules&camelCase&localIdentName=nm-[name]__[local]&importLoaders=2',
+                            'resolve-url-loader?sourceMap',
+                            'sass-loader?sourceMap'
+                        ],
+                        publicPath: ''
+                    })
                 }
             ]
         },
@@ -72,11 +72,12 @@ module.exports = function(devMode, hot) {
                     'NODE_ENV': devMode ? '"development"' : '"production"'
                 }
             }),
-            new ExtractTextPlugin("nationalmap.css", {disable: hot, ignoreOrder: true})
+            new ExtractTextPlugin({filename: "nationalmap.css", disable: hot, ignoreOrder: true})
         ],
        resolve: {
-            alias: {}
-        }        
+            alias: {},
+            modules: ['node_modules']
+        }
     };
     config.resolve.alias['terriajs-variables'] = require.resolve('../lib/Styles/variables.scss');
     return configureWebpackForTerriaJS(path.dirname(require.resolve('terriajs/package.json')), config, devMode, hot, ExtractTextPlugin);

TerriaJS/TerriaMap

buildprocess/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = function(devMode, hot) {
     var config = {
         entry: './index.js',
         output: {
-            path: 'wwwroot/build',
+            path: path.resolve(__dirname, 'wwwroot', 'build'),
             filename: 'TerriaMap.js',
             // work around chrome needing the full URL when using sourcemaps (http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809)
             publicPath: hot ? 'http://localhost:3003/build/' : 'build/',
@@ -22,7 +22,7 @@ module.exports = function(devMode, hot) {
                 {
                     test: /\.html$/,
                     include: path.resolve(__dirname, '..', 'lib', 'Views'),
-                    loader: require.resolve('raw-loader')
+                    loader: 'raw-loader'
                 },
                 {
                     test: /\.(js|jsx)$/,
@@ -30,19 +30,19 @@ module.exports = function(devMode, hot) {
                         path.resolve(__dirname, '..', 'index.js'),
                         path.resolve(__dirname, '..', 'lib')
                     ],
-                    loader: require.resolve('babel-loader'),
+                    loader: 'babel-loader',
                     query: {
                         sourceMap: false, // generated sourcemaps are currently bad, see https://phabricator.babeljs.io/T7257
                         presets: ['es2015', 'react'],
                         plugins: [
-                            require.resolve('jsx-control-statements')
+                            'jsx-control-statements'
                         ]
                     }
                 },
                 {
                     test: /\.(png|jpg|svg|gif)$/,
                     include: path.resolve(__dirname, '..', 'wwwroot', 'images'),
-                    loader: require.resolve('url-loader'),
+                    loader: 'url-loader',
                     query: {
                         limit: 8192
                     }
@@ -50,19 +50,19 @@ module.exports = function(devMode, hot) {
                 {
                     test: /\.scss$/,
                     include: [path.resolve(__dirname, '..', 'lib')],
-                    loader: hot ?
-                        require.resolve('style-loader') + '!' +
-                        require.resolve('css-loader') + '?sourceMap&modules&camelCase&localIdentName=tm-[name]__[local]&importLoaders=2!' +
-                        require.resolve('resolve-url-loader') + '?sourceMap!' +
-                        require.resolve('sass-loader') + '?sourceMap'
-                     : ExtractTextPlugin.extract(
-                        require.resolve('css-loader') + '?sourceMap&modules&camelCase&localIdentName=tm-[name]__[local]&importLoaders=2!' +
-                        require.resolve('resolve-url-loader') + '?sourceMap!' +
-                        require.resolve('sass-loader') + '?sourceMap',
-                        {
-                            publicPath: ''
-                        }
-                    )
+                    loader: hot ? [
+                        'style-loader',
+                        'css-loader?sourceMap&modules&camelCase&localIdentName=tm-[name]__[local]&importLoaders=2',
+                        'resolve-url-loader?sourceMap',
+                        'sass-loader?sourceMap'
+                    ] : ExtractTextPlugin.extract({
+                        use: [
+                            'css-loader?sourceMap&modules&camelCase&localIdentName=tm-[name]__[local]&importLoaders=2',
+                            'resolve-url-loader?sourceMap',
+                            'sass-loader?sourceMap'
+                        ],
+                        publicPath: ''
+                    })
                 }
             ]
         },
@@ -72,11 +72,12 @@ module.exports = function(devMode, hot) {
                     'NODE_ENV': devMode ? '"development"' : '"production"'
                 }
             }),
-            new ExtractTextPlugin("TerriaMap.css", {disable: hot, ignoreOrder: true})
+            new ExtractTextPlugin({filename: "TerriaMap.css", disable: hot, ignoreOrder: true})
         ],
        resolve: {
-            alias: {}
-        }        
+            alias: {},
+            modules: ['node_modules']
+        }
     };
     config.resolve.alias['terriajs-variables'] = require.resolve('../lib/Styles/variables.scss');
     return configureWebpackForTerriaJS(path.dirname(require.resolve('terriajs/package.json')), config, devMode, hot, ExtractTextPlugin);