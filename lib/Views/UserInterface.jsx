b171d72befc46ddba2ce70b0754c6b9b2b18692a
Ashley Stacey <ashley.stacey@data61.csiro.au>
2017-03-21 16:17:48 +1100

    Add experimental feature AugmentedVirtuality.

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -5,7 +5,8 @@ import version from '../../version';
 import StandardUserInterface from 'terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
 import MenuItem from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
 import RelatedMaps from './RelatedMaps';
-import { Menu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
+import { Menu, ExperimentalMenu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
+import AugmentedVirtualityTool from 'terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool.jsx';
 
 import './global.scss';
 
@@ -16,6 +17,9 @@ export default function UserInterface(props) {
                 <RelatedMaps viewState={props.viewState} />
                 <MenuItem caption="About" href="about.html" key="about-link"/>
             </Menu>
+            <ExperimentalMenu>
+                <AugmentedVirtualityTool viewState={props.viewState} terria={props.viewState.terria} experimentalWarning={true} key="augmented-virtuality-tool"/>
+            </ExperimentalMenu>
         </StandardUserInterface>
     );
 }
