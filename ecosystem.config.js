2281553052a6165b99df9598bb23425225934e58
Kevin Ring <kevin@kotachrome.com>
2019-05-16 17:16:37 +1000

    Support args from npm start.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,19 @@
 /*global __dirname*/
 const path = require('path');
 
+// Pass through additional arguments that might ultimately have come from
+// something like `npm run start -- --port 3009`
+const argpos = process.argv.indexOf('--');
+const args = argpos > -1 ? process.argv.slice(argpos + 1) : [];
+
 module.exports = {
   apps : [{
     name: path.basename(__dirname),
     script: require.resolve('terriajs-server'),
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: '--config-file devserverconfig.json',
+    // passed to app, so any valid arguments in options.js are allowed.
+    args: args.join(' '),
     instances: 1,
     autorestart: true,
     watch: false,
