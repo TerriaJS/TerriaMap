32f68521e4b7c2da8c91504a94e48d3da3de8e20
Wing Ho <mrwingh@gmail.com>
2019-06-03 13:45:48 +1000

    Apply prettier

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -1,46 +1,59 @@
-import { Menu, Nav, ExperimentalMenu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
-import MeasureTool from 'terriajs/lib/ReactViews/Map/Navigation/MeasureTool';
-import MenuItem from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
-import PropTypes from 'prop-types';
-import React from 'react';
-import RelatedMaps from './RelatedMaps';
-import SplitPoint from 'terriajs/lib/ReactViews/SplitPoint';
-import StandardUserInterface from 'terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
-import version from '../../version';
+import {
+  Menu,
+  Nav,
+  ExperimentalMenu
+} from "terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups";
+import MeasureTool from "terriajs/lib/ReactViews/Map/Navigation/MeasureTool";
+import MenuItem from "terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem";
+import PropTypes from "prop-types";
+import React from "react";
+import RelatedMaps from "./RelatedMaps";
+import SplitPoint from "terriajs/lib/ReactViews/SplitPoint";
+import StandardUserInterface from "terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx";
+import version from "../../version";
 
-import './global.scss';
+import "./global.scss";
 
 function loadAugmentedVirtuality(callback) {
-    require.ensure('terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool', () => {
-        const AugmentedVirtualityTool = require('terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool');
-        callback(AugmentedVirtualityTool);
-    }, 'AugmentedVirtuality');
+  require.ensure(
+    "terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool",
+    () => {
+      const AugmentedVirtualityTool = require("terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool");
+      callback(AugmentedVirtualityTool);
+    },
+    "AugmentedVirtuality"
+  );
 }
 
 function isBrowserSupportedAV() {
-    return /Android|iPhone|iPad/i.test(navigator.userAgent);
+  return /Android|iPhone|iPad/i.test(navigator.userAgent);
 }
 
 export default function UserInterface(props) {
-    return (
-        <StandardUserInterface {... props} version={version}>
-            <Menu>
-                <RelatedMaps viewState={props.viewState} />
-                <MenuItem caption="About" href="about.html" key="about-link"/>
-            </Menu>
-            <Nav>
-                <MeasureTool terria={props.viewState.terria} key="measure-tool"/>
-            </Nav>
-            <ExperimentalMenu>
-                <If condition={isBrowserSupportedAV()}>
-                    <SplitPoint loadComponent={loadAugmentedVirtuality} viewState={props.viewState} terria={props.viewState.terria} experimentalWarning={true}/>
-                </If>
-            </ExperimentalMenu>
-        </StandardUserInterface>
-    );
+  return (
+    <StandardUserInterface {...props} version={version}>
+      <Menu>
+        <RelatedMaps viewState={props.viewState} />
+        <MenuItem caption="About" href="about.html" key="about-link" />
+      </Menu>
+      <Nav>
+        <MeasureTool terria={props.viewState.terria} key="measure-tool" />
+      </Nav>
+      <ExperimentalMenu>
+        <If condition={isBrowserSupportedAV()}>
+          <SplitPoint
+            loadComponent={loadAugmentedVirtuality}
+            viewState={props.viewState}
+            terria={props.viewState.terria}
+            experimentalWarning={true}
+          />
+        </If>
+      </ExperimentalMenu>
+    </StandardUserInterface>
+  );
 }
 
 UserInterface.propTypes = {
-    terria: PropTypes.object,
-    viewState: PropTypes.object
+  terria: PropTypes.object,
+  viewState: PropTypes.object
 };
