e4eea9444e13c9448f216650b1b49059250cdc96
Wing Ho <mrwingh@gmail.com>
2019-06-24 17:28:27 +1000

    Apply prettier

--- lib/Views/render.jsx
+++ lib/Views/render.jsx
@@ -1,25 +1,24 @@
-import ReactDOM from 'react-dom';
-import RedBox from 'redbox-react';
-import React from 'react';
+import ReactDOM from "react-dom";
+import RedBox from "redbox-react";
+import React from "react";
 
 export default function renderUi(terria, allBaseMaps, viewState) {
   let render = () => {
-    const UI = require('./UserInterface').default;
-    ReactDOM.render(<UI terria={terria} allBaseMaps={allBaseMaps}
-                        viewState={viewState}/>, document.getElementById('ui'));
+    const UI = require("./UserInterface").default;
+    ReactDOM.render(
+      <UI terria={terria} allBaseMaps={allBaseMaps} viewState={viewState} />,
+      document.getElementById("ui")
+    );
   };
 
   if (module.hot && process.env.NODE_ENV !== "production") {
     // Support hot reloading of components
     // and display an overlay for runtime errors
     const renderApp = render;
-    const renderError = (error) => {
+    const renderError = error => {
       console.error(error);
       console.error(error.stack);
-      ReactDOM.render(
-        <RedBox error={error}/>,
-        document.getElementById('ui')
-      );
+      ReactDOM.render(<RedBox error={error} />, document.getElementById("ui"));
     };
     render = () => {
       try {
@@ -28,7 +27,7 @@ export default function renderUi(terria, allBaseMaps, viewState) {
         renderError(error);
       }
     };
-    module.hot.accept('./UserInterface', () => {
+    module.hot.accept("./UserInterface", () => {
       setTimeout(render);
     });
   }
