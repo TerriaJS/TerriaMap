79bfc27e883ead2104a274641a73f6d2a4df0f9c
Kevin Ring <kevin@kotachrome.com>
2020-04-08 23:52:07 +1000

    Working ogc3d reference, add other services to catalog.

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ import render from './lib/Views/render';
 import createGlobalBaseMapOptions from 'terriajs/lib/ViewModels/createGlobalBaseMapOptions';
 import registerCatalogMembers from 'terriajs/lib/Models/registerCatalogMembers';
 import Ogc3dContainer from './lib/Models/Ogc3dContainerReference';
+import CatalogMemberFactory from 'terriajs/lib/Models/CatalogMemberFactory';
 
 // Register all types of catalog members in the core TerriaJS.  If you only want to register a subset of them
 // (i.e. to reduce the size of your application if you don't actually use them all), feel free to copy a subset of
--- lib/Models/Ogc3dContainerReference.ts
+++ lib/Models/Ogc3dContainerReference.ts
@@ -1,14 +1,17 @@
+import filterOutUndefined from "terriajs/lib/Core/filterOutUndefined";
+import loadJson from "terriajs/lib/Core/loadJson";
 import ReferenceMixin from "terriajs/lib/ModelMixins/ReferenceMixin";
 import UrlMixin from "terriajs/lib/ModelMixins/UrlMixin";
+import CatalogGroup from "terriajs/lib/Models/CatalogGroupNew";
+import Cesium3DTilesCatalogItem from "terriajs/lib/Models/Cesium3DTilesCatalogItem";
+import CommonStrata from "terriajs/lib/Models/CommonStrata";
 import CreateModel from "terriajs/lib/Models/CreateModel";
 import { BaseModel } from "terriajs/lib/Models/Model";
+import proxyCatalogItemUrl from "terriajs/lib/Models/proxyCatalogItemUrl";
+import Terria from "terriajs/lib/Models/Terria";
 import CatalogMemberReferenceTraits from "terriajs/lib/Traits/CatalogMemberReferenceTraits";
 import mixTraits from "terriajs/lib/Traits/mixTraits";
 import UrlTraits from "terriajs/lib/Traits/UrlTraits";
-import proxyCatalogItemUrl from "terriajs/lib/Models/proxyCatalogItemUrl";
-import loadJson from "terriajs/lib/Core/loadJson";
-import { JsonObject, JsonArray } from "terriajs/lib/Core/Json";
-import CatalogGroup from "terriajs/lib/Models/CatalogGroupNew";
 
 export class Ogc3dContainerReferenceTraits extends mixTraits(
   CatalogMemberReferenceTraits,
@@ -18,17 +21,107 @@ export class Ogc3dContainerReferenceTraits extends mixTraits(
 export default class Ogc3dContainerReference extends UrlMixin(
   ReferenceMixin(CreateModel(Ogc3dContainerReferenceTraits))
 ) {
-  protected forceLoadReference(previousTarget: BaseModel): Promise<BaseModel> {
+  static readonly type = "ogc3d";
+
+  get type() {
+    return Ogc3dContainerReference.type;
+  }
+
+  protected forceLoadReference(
+    previousTarget: BaseModel
+  ): Promise<BaseModel | undefined> {
+    if (this.uri === undefined) {
+      return Promise.resolve(undefined);
+    }
+
+    const id = this.uniqueId;
+    const name = this.name;
+
     const proxiedUrl = proxyCatalogItemUrl(this, this.uri.toString(), "0d");
     return loadJson(proxiedUrl).then(json => {
       if (json.collections !== undefined) {
-        return this.createGroup(json, previousTarget);
+        return Ogc3dContainerReference.loadFromCollections(
+          this.terria,
+          this,
+          id,
+          name,
+          json,
+          previousTarget
+        );
+      } else if (json.links !== undefined) {
+        return Ogc3dContainerReference.loadFromLandingPage(
+          this.terria,
+          this,
+          id,
+          name,
+          json,
+          previousTarget
+        );
       }
       return undefined;
     });
   }
 
-  private createGroup(
+  private static loadFromLandingPage(
+    terria: Terria,
+    sourceReference: BaseModel | undefined,
+    id: string | undefined,
+    name: string | undefined,
+    json: any,
+    previousTarget: BaseModel
+  ): Promise<BaseModel | undefined> {
+    const links = json.links;
+    const dataLinks = links.filter(
+      (link: any) => link.rel === "data" && link.href !== undefined
+    );
+    if (dataLinks.length === 0) {
+      return Promise.resolve(undefined);
+    }
+
+    const firstDataLink = dataLinks[0];
+    const proxiedUrl = proxyCatalogItemUrl(
+      sourceReference,
+      firstDataLink.href,
+      "0d"
+    );
+    return loadJson(proxiedUrl).then(json => {
+      if (json.collections !== undefined) {
+        return Ogc3dContainerReference.loadFromCollections(
+          terria,
+          sourceReference,
+          id,
+          name,
+          json,
+          previousTarget
+        );
+      }
+      return undefined;
+    });
+  }
+
+  private static loadFromCollections(
+    terria: Terria,
+    sourceReference: BaseModel | undefined,
+    id: string | undefined,
+    name: string | undefined,
+    json: any,
+    previousTarget: BaseModel
+  ): Promise<BaseModel | undefined> {
+    return Ogc3dContainerReference.loadGroup(
+      terria,
+      sourceReference,
+      id,
+      name,
+      json,
+      previousTarget
+    );
+  }
+
+  private static loadGroup(
+    terria: Terria,
+    sourceReference: BaseModel | undefined,
+    id: string | undefined,
+    name: string | undefined,
     json: any,
     previousTarget: BaseModel
   ): Promise<BaseModel> {
@@ -36,10 +129,51 @@ export default class Ogc3dContainerReference extends UrlMixin(
     if (previousTarget && previousTarget instanceof CatalogGroup) {
       group = previousTarget;
     } else {
-      group = new CatalogGroup(this.uniqueId, this.terria, this);
+      group = new CatalogGroup(id, terria, sourceReference);
     }
 
+    group.setTrait(CommonStrata.definition, "name", name);
+
     const collections = json.collections;
+
+    const ids = filterOutUndefined(
+      collections.map((collection: any) => {
+        const collectionId = id + "/" + collection.id;
+        const links = collection.links;
+        if (links === undefined) {
+          return undefined;
+        }
+
+        const compatibleLinks = links.filter(
+          (link: any) => link.type === "application/json+3dtiles"
+        );
+        if (compatibleLinks.length === 0) {
+          return undefined;
+        }
+
+        const existing = terria.getModelById(BaseModel, collectionId);
+        const model =
+          existing && existing.type === Cesium3DTilesCatalogItem.type
+            ? existing
+            : new Cesium3DTilesCatalogItem(collectionId, terria);
+
+        if (existing === undefined) {
+          terria.addModel(model);
+        }
+
+        model.setTrait(CommonStrata.definition, "name", collection.title);
+        model.setTrait(
+          CommonStrata.definition,
+          "description",
+          collection.description
+        );
+        model.setTrait(CommonStrata.definition, "url", compatibleLinks[0].href);
+
+        return model.uniqueId;
+      })
+    );
+
+    group.setTrait(CommonStrata.definition, "members", ids);
 
     return Promise.resolve(group);
   }
