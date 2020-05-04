1167c584e00b8da3d4317098babfbe0a0095bfd4
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-05-04 15:55:56 +1000

    ydyr stuff

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ import BingMapsSearchProviderViewModel from 'terriajs/lib/Models/BingMapsSearchP
 import render from './lib/Views/render';
 import createGlobalBaseMapOptions from 'terriajs/lib/ViewModels/createGlobalBaseMapOptions';
 import registerCatalogMembers from 'terriajs/lib/Models/registerCatalogMembers';
+import CatalogMemberFactory from "terriajs/lib/Models/CatalogMemberFactory";
+import CsvCatalogItemOverride from "./lib/Models/CsvCatalogItemOverride";
 
 // Register all types of catalog members in the core TerriaJS.  If you only want to register a subset of them
 // (i.e. to reduce the size of your application if you don't actually use them all), feel free to copy a subset of
@@ -48,6 +50,7 @@ const viewState = new ViewState({
 });
 
 registerCatalogMembers();
+CatalogMemberFactory.register(CsvCatalogItemOverride.type, CsvCatalogItemOverride)
 
 if (process.env.NODE_ENV === "development") {
     window.viewState = viewState;
---
+++ lib/Models/CsvCatalogItemOverride.ts
@@ -0,0 +1,18 @@
+import CsvCatalogItem from "terriajs/lib/Models/CsvCatalogItem";
+import Terria from "terriajs/lib/Models/Terria";
+import { BaseModel } from "terriajs/lib/Models/Model";
+import CommonStrata from "terriajs/lib/Models/CommonStrata";
+
+export default class CsvCatalogItemOverride extends CsvCatalogItem {
+  constructor(
+    id: string | undefined,
+    terria: Terria,
+    sourceReference?: BaseModel
+  ) {
+    super(id, terria);
+
+    this.setTrait(CommonStrata.underride, "enableManualRegionMapping", true);
+
+    console.log(this);
+  }
+}
