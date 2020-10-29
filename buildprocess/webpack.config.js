0a176667005949540a0283dd0ce4f457b71395d2
Wing Ho <mrwingh@gmail.com>
2020-10-29 14:00:17 +1100

    Restore sane defaults

--- buildprocess/webpack.config.js
+++ buildprocess/webpack.config.js
@@ -227,7 +227,7 @@ module.exports = function(webpack, devMode, hot) {
                 // If you run out of memory, try a lower value here
                 maxConcurrentRoutes: 12,
                 // headless: false, // set to false for debugging
-                // timeout: 3000
+                timeout: 5000
             }),
             postProcess(context) {
                 // Hide any errors or popups in the rendered page.
