cf3fd7bb40590472bb671477e36eb96880445502
Sergio Rocha <sergiorocha@Sergio-Rocha-MacBook-Pro.local>
2021-10-28 13:38:55 -0500

    Created a search by type component/view

---
+++ lib/Views/SearchByType.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import MenuPanel from "terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuPanel.jsx";
+import PanelStyles from "terriajs/lib/ReactViews/Map/Panels/panel.scss";
+import Styles from "./related-maps.scss";
+import classNames from "classnames";
+
+function SearchByType(props) {
+  const dropdownTheme = {
+    inner: Styles.dropdownInner,
+    icon: "gallery"
+  };
+
+  return (
+    <MenuPanel
+      theme={dropdownTheme}
+      btnText="Search By Type"
+      smallScreen={props.smallScreen}
+      viewState={props.viewState}
+      btnTitle="See types"
+      showDropdownInCenter
+    >
+      <div className={classNames(PanelStyles.header)}>
+        <label className={PanelStyles.heading}>Search by type</label>
+      </div>
+    </MenuPanel>
+  );
+}
+
+SearchByType.propTypes = {
+  viewState: PropTypes.object.isRequired,
+  smallScreen: PropTypes.bool
+};
+
+export default SearchByType;
