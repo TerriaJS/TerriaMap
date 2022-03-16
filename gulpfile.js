3be96916191acea70e7d5879faae80b5efbffd0d
Stephen Davies <steve9164@gmail.com>
2022-03-16 14:23:37 +1100

    Add render-index to release task and add changelog entry

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ gulp.task('build-app', gulp.parallel('render-index', gulp.series('check-terriajs
     runWebpack(webpack, webpackConfig, done);
 })));
 
-gulp.task('release-app', gulp.series('check-terriajs-dependencies', 'write-version', function releaseApp(done) {
+gulp.task('release-app', gulp.parallel('render-index', gulp.series('check-terriajs-dependencies', 'write-version', function releaseApp(done) {
     var runWebpack = require('terriajs/buildprocess/runWebpack.js');
     var webpack = require('webpack');
     var webpackConfig = require('./buildprocess/webpack.config.js')(false);
@@ -79,7 +79,7 @@ gulp.task('release-app', gulp.series('check-terriajs-dependencies', 'write-versi
     runWebpack(webpack, Object.assign({}, webpackConfig, {
         plugins: webpackConfig.plugins || []
     }), done);
-}));
+})));
 
 gulp.task('watch-render-index', gulp.series('render-index', function watchRenderIndex() {
   return gulp.watch(['wwwroot/index.ejs'], gulp.series('render-index'));
