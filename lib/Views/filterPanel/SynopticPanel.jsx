e08f595d5fcf4cc02d213b4d51f11f41e0cd60d6
Lisa Stillwell <lisa@renci.org>
2023-03-03 17:27:38 -0500

    some more work on FilterPanel

---
+++ lib/Views/filterPanel/SynopticPanel.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import CommonPanel from "./CommonPanel";
+
+export default function SynopticPanel() {
+  const [date, setDate] = React.useState(null);
+  const [cycle, setCycle] = React.useState("");
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const handleCycleChange = (event) => {
+    setCycle(event.target.value);
+  };
+
+  return (
+    <FormControl>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Run Date"
+          value={date}
+          onChange={(newValue) => {
+            setDate(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Cycle</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={cycle}
+            label="Cycle"
+            onChange={handleCycleChange}
+          >
+            <MenuItem value={0}>00</MenuItem>
+            <MenuItem value={6}>06</MenuItem>
+            <MenuItem value={12}>12</MenuItem>
+            <MenuItem value={18}>18</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <CommonPanel />
+    </FormControl>
+  );
+}
