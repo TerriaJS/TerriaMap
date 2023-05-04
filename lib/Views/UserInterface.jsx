d822031fb7343012f3c30b6d448e41c2d91eafaf
Sergio Rocha <rochascript@gmail.com>
2023-05-04 11:09:28 -0600

    Made edits to synoptic form to be able to access the web api, form now returns proper layers

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 // import RelatedMaps from "terriajs/lib/ReactViews/RelatedMaps/RelatedMaps";
 import StandardUserInterface from "terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface";
 import version from "../../version";
@@ -11,18 +11,23 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LayersProvider } from "../custom-ui/context";
 import { Layout } from "../custom-ui/components/layout";
-import { BaseMap } from "../custom-ui/components/map";
+// import { useLayers } from "../custom-ui/context";
 
 export default function UserInterface(props) {
   const [open, setOpen] = React.useState(false);
   const [layerData, setLayerData] = useState("default data");
   const [selectedLayers, setSelectedLayers] = useState([]);
+  // const [ setLayers ] = useLayers();
   // const value = useMemo(() => ({ layerData, setLayertData }), [layerData]);
   // console.log(props.viewState);
   // const relatedMaps = props.viewState.terria.configParameters.relatedMaps;
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
+  console.log(props.viewState.terria.workbench.items);
+  // useEffect(() => {
+  //   setLayers([props.viewState.terria.workbench.items])
+  // }, []);
 
   // setSelectedLayers([...selectedLayers, viewState.terria.workbench.items]);
 
@@ -40,8 +45,7 @@ export default function UserInterface(props) {
 
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <LayersProvider>
-            <Layout>
-              {/* <BaseMap /> */}
+            <Layout viewState={props.viewState}>
               <StandardUserInterface
                 {...props}
                 version={version}
