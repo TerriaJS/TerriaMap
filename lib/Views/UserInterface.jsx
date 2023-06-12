0d4d723085f94b3736bbe21098afcae3e4e012a1
Sergio Rocha <rochascript@gmail.com>
2023-06-12 13:42:13 -0500

    fixed opacity scale issue where the state persisted amongst different layers

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -1,9 +1,8 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import RelatedMaps from "terriajs/lib/ReactViews/RelatedMaps/RelatedMaps";
 import StandardUserInterface from "terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface";
 import version from "../../version";
-import { Context } from "../context/context";
 import "./global.scss";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -11,31 +10,19 @@ import { LayersProvider } from "../custom-ui/context";
 import { Layout } from "../custom-ui/components/layout";
 
 export default function UserInterface(props) {
-  const [layerData, setLayerData] = useState("default data");
-  const [selectedLayers, setSelectedLayers] = useState([]);
-
   return (
     <>
-      <Context.Provider
-        value={{
-          layers: layerData,
-          setLayerData: setLayerData,
-          selectedLayers: selectedLayers,
-          setSelectedLayers: setSelectedLayers
-        }}
-      >
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <LayersProvider>
-            <Layout viewState={props.viewState}>
-              <StandardUserInterface
-                {...props}
-                version={version}
-                style={{ maxHeight: "400px !important" }}
-              />
-            </Layout>
-          </LayersProvider>
-        </LocalizationProvider>
-      </Context.Provider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LayersProvider>
+          <Layout viewState={props.viewState}>
+            <StandardUserInterface
+              {...props}
+              version={version}
+              style={{ maxHeight: "400px !important" }}
+            />
+          </Layout>
+        </LayersProvider>
+      </LocalizationProvider>
     </>
   );
 }
