671deb4d44a678e1e0ce34f1a90581261f3a377e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-02-20 00:20:58 +0100

    tsconfig: extend from node_modules
    There is no "terriajs" directory
    in this repository, so import
    tsconfig.json from the terriajs
    directory inside node_modules
    instead, just like the ESLint
    configuration does.
    
    This requires re-defining outDir,
    typeRoots, and include to the same
    values that they have in terriajs,
    since they will otherwise be relative
    to the node_modules tsconfig.json.
    
    The types option requires including
    a couple of lib/ThirdParty directories
    in terriajs.
    
    The maxNodeModuleJsDepth parameter
    is required for not getting errors
    about implicit any types on jsx
    imports in terriajs source code.

---
+++ lib/ThirdParty/styled-components/index.d.ts
@@ -0,0 +1,24 @@
+// Copy of lib/ThirdParty/styled-components from terriajs, but with updated
+// import path for terriaTheme.
+
+// Pull in the css prop type addition to react attributes
+/// <reference types="styled-components/cssprop" />
+
+// import your custom theme
+import { CSSProp } from "styled-components";
+import { terriaTheme } from "terriajs/lib/ReactViews/StandardUserInterface/StandardTheme";
+
+type Theme = typeof terriaTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends Theme {}
+}
+
+// See https://github.com/styled-components/styled-components/issues/2528
+// .css isn't included in @types/styled-components because it has to be enabled
+// through a babel plugin or macro
+declare module "react" {
+  interface Attributes {
+    css?: CSSProp;
+  }
+}
