084b7ea00c00be2a5c7e35d066587f4038b9d65a
Zoran Kokeza <zoran.kokeza@terria.io>
2025-07-04 17:03:17 +0200

    update after review

--- lib/Views/render.jsx
+++ lib/Views/render.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { flushSync } from "react-dom";
-
 import Variables from "../Styles/variables.scss";
 import UI from "./UserInterface";
 
@@ -8,6 +7,7 @@ export default function renderUi(terria, allBaseMaps, viewState) {
   const container = document.getElementById("ui");
   const root = createRoot(container);
 
+  // Ensure that the initial render is synchronous so there is no a white flash visible between loading and rendering
   flushSync(() =>
     root.render(
       <UI
